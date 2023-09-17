7c543d2a9461ecb0a9b4a58adb011809563657e0
Matthew Hoff <matthew.g.hoff@gmail.com>
2023-09-17 13:08:29 -0400

    check for EIA API key on package load/attachment

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,23 @@
-.onLoad <- function(libname, pkgname){
+.onAttach <- function(libname, pkgname) {
   options(eia_antidos = 1)
+  key = Sys.getenv("EIA_KEY")
+  if(key == "") {
+    x <- "EIA_KEY"
+  } else {
+    x <- ""
+  }
+  if(x == ""){
+    packageStartupMessage("EIA_KEY found in Renviron.")
+  } else {
+    packageStartupMessage(
+      x, " not found in Renviron. Please add and restart R.\n",
+      "You can edit your Renviron file with usethis::edit_r_environ()."
+    )
+    if(identical(find.package("usethis", quiet = TRUE), character(0))) {
+      packageStartupMessage("Install usethis with install.packages(\"usethis\").")
+    }
+  }
+  invisible()
 }
 
 .session_eia_env <- new.env()
