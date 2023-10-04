9a38b3186747407a8ca007538c1c498f9d97e5c0
Matthew Hoff <matthew.g.hoff@gmail.com>
2023-10-04 13:03:15 -0400

    if-else cleanup

--- R/zzz.R
+++ R/zzz.R
@@ -1,19 +1,15 @@
 .onAttach <- function(libname, pkgname) {
   options(eia_antidos = 1) # not sure yet what "antidos" is...
   key = Sys.getenv("EIA_KEY")
-  if(key == "") {
-    x <- "EIA_KEY"
-  } else {
-    x <- ""
-  }
+  x <- if(key == "") "EIA_KEY" else ""
   if(x == ""){
     packageStartupMessage("EIA_KEY found in Renviron.")
   } else {
     packageStartupMessage(
       x, " not found in Renviron.\n",
       "Please instantiate with eia::eia_set_key(<YOUR_KEY>), or...\n",
-      "edit your Renviron file with usethis::edit_r_environ(),",
-      " then restart R."
+      "edit your Renviron file with usethis::edit_r_environ(), ",
+      "then restart R."
     )
     if(identical(find.package("usethis", quiet = TRUE), character(0))) {
       packageStartupMessage("Install usethis with install.packages(\"usethis\").")
