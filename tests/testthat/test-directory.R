42960ce423029c62ab8638b59c128b6157f9ef2f
Matthew Hoff <matthew.g.hoff@gmail.com>
2023-10-10 09:55:18 -0400

    "directory(ies)" > "categories"; update test::* with new 3rd edition functions

--- tests/testthat/test-directory.R
+++ tests/testthat/test-directory.R
@@ -1,4 +1,4 @@
-context("categories")
+context("directory")
 
 options(eia_antidos = 0)
 suppressWarnings(key <- eia_get_key())
@@ -6,24 +6,24 @@ suppressWarnings(key <- eia_get_key())
 test_that("category functions returns as expected", {
   if(is.null(key)) skip("API key not available.")
 
-  x <- eia_categories(tidy = FALSE, key = key)
-  expect_is(x, "list")
+  x <- eia_directory(tidy = FALSE, key = key)
+  expect_type(x, "list")
   expect_equal(names(x), c("response", "request", "apiVersion"))
 
-  x2 <- eia_categories(tidy = FALSE)
+  x2 <- eia_directory(tidy = FALSE)
   expect_identical(x, x2)
-  x2 <- eia_categories(tidy = FALSE, cache = FALSE)
+  x2 <- eia_directory(tidy = FALSE, cache = FALSE)
   expect_identical(x, x2)
 
-  x <- eia_categories(tidy = NA)
-  expect_is(x, "character")
+  x <- eia_directory(tidy = NA)
+  expect_type(x, "character")
 
-  x <- eia_categories()
-  expect_is(x, "tbl_df")
+  x <- eia_directory()
+  expect_s3_class(x, "tbl_df")
   expect_equal(dim(x), c(14, 3))
   expect_equal(names(x), c("id", "name", "description"))
 
-  x <- eia_subcategories("electricity")
-  expect_is(x, "tbl_df")
+  x <- eia_directory("electricity")
+  expect_s3_class(x, "tbl_df")
   expect_equal(dim(x), c(6, 3))
 })
