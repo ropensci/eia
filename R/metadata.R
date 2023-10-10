f20965921d877e7cd525eae400438fde4f6768ac
Matthew Hoff <matthew.g.hoff@gmail.com>
2023-10-10 15:05:16 -0400

    add functions for obtaining metadata, specific facet values, and ultimately the data itself

---
+++ R/metadata.R
@@ -0,0 +1,199 @@
+#' EIA metadata
+#'
+#' Obtain EIA data metadata
+#'
+#' By default, additional processing is done to return a list containing tibble data frames.
+#' Set `tidy = FALSE` to return only the initial list result of `jsonlite::fromJSON`.
+#' Set `tidy = NA` to return the original JSON as a character string.
+#'
+#' Set to `cache = FALSE` to force a new API call for updated data.
+#' Using `FALSE` always makes a new API call and returns the result from the server.
+#' `TRUE` uses memoization on a per R session basis, caching the result of the
+#' function call in memory for the duration of the R session.
+#' You can reset the entire cache by calling `eia_clear_cache`.
+#'
+#' @param dir character, directory path.
+#' @param tidy logical, return a tidier result. See details.
+#' @param cache logical, cache result for duration of R session using memoization. See details.
+#' @param key API key: character if set explicitly; not needed if key is set globally. See `eia_set_key()`.
+#'
+#' @return if `tidy = TRUE`, then `NULL`; if `tidy = NA` or `tidy = FALSE`, then
+#' JSON the latter of which is a list result from `jsonlite::fromJSON`.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' eia_directory("electricity/retail-sales")
+#' eia_metadata("electricity/retail-sales")
+#' }
+eia_metadata <- function(dir, tidy = TRUE, cache = TRUE, key = eia_get_key()){
+  .key_check(key)
+  if(cache) .eia_metadata_memoized(dir, tidy, key) else .eia_metadata(dir, tidy, key)
+}
+
+.eia_metadata_url <- function(dir, key){
+  .eia_url(path = paste0(dir, "/?api_key=", key))
+}
+
+.eia_metadata_values <- function(data){
+  d <- sapply(names(data), function(x){
+    unlist(list("data" = x, data[[x]]))
+  })
+  tibble::as_tibble(t(d))
+}
+
+.eia_metadata <- function(dir, tidy, key){
+  r <- .eia_metadata_url(dir, key) |> .eia_get()
+  if(is.na(tidy)) return(r)
+  r <- jsonlite::fromJSON(r)
+  if(!tidy) return(r)
+  if(tidy){
+    name <- if(!is.null(r$response$name)) r$response$name else r$response$id
+    cat("Name:\n  ", name)
+    if(!is.null(r$response$description)){
+      cat("\n\nDescription:\n  ", r$response$description)
+    }
+    if (!is.null(r$response$data)){
+      cat("\n\nData Values:\n  "); print(.eia_metadata_values(r$response$data))
+    }
+    if (!is.null(r$response$facets)){
+      cat("\nFacets:\n  "); print(tibble::as_tibble(r$response$facets))
+    }
+    if (!is.null(r$response$frequency)){
+      cat("\nFrequency:\n  "); print(tibble::as_tibble(r$response$frequency))
+    }
+    cat("\nDefaults:\n  ")
+    cat("Date Format:", r$response$defaultDateFormat, "\n  Frequency:", r$response$defaultFrequency)
+    cat("\n\nDate Range: ", r$response$startPeriod, "to", r$response$endPeriod)
+    invisible(NULL)
+  }
+}
+
+.eia_metadata_memoized <- memoise::memoise(.eia_metadata)
+
+
+
+#' EIA facets
+#'
+#' Obtain facets for a given set of EIA data.
+#'
+#' By default, additional processing is done to return a list containing tibble data frames.
+#' Set `tidy = FALSE` to return only the initial list result of `jsonlite::fromJSON`.
+#' Set `tidy = NA` to return the original JSON as a character string.
+#'
+#' Set to `cache = FALSE` to force a new API call for updated data.
+#' Using `FALSE` always makes a new API call and returns the result from the server.
+#' `TRUE` uses memoization on a per R session basis, caching the result of the
+#' function call in memory for the duration of the R session.
+#' You can reset the entire cache by calling `eia_clear_cache`.
+#'
+#' @param dir character, directory path.
+#' @param facet character
+#' @param tidy logical, return a tidier result. See details.
+#' @param cache logical, cache result for duration of R session using memoization. See details.
+#' @param key API key: character if set explicitly; not needed if key is set globally. See `eia_set_key()`.
+#'
+#' @return if `tidy = TRUE`, then `data.frame/tibble`; f `tidy = NA` or `tidy = FALSE`, then
+#' JSON the latter of which is a list result from `jsonlite::fromJSON`.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' eia_facets("electricity/retail-sales", facet = "sectorid")
+#' }
+eia_facets <- function(dir, facet, tidy = TRUE, cache = TRUE, key = eia_get_key()){
+  .key_check(key)
+  if(cache) .eia_facet_memoized(dir, facet, tidy, key) else .eia_facets(dir, facet, tidy, key)
+}
+
+.eia_facet_url <- function(dir, facet, key){
+  .eia_url(path = paste0(dir, "/facet/", facet, "/?api_key=", key))
+}
+
+.eia_facets <- function(dir, facet, tidy, key){
+  r <- .eia_facet_url(dir, facet, key) |> .eia_get()
+  if(is.na(tidy)) return(r)
+  r <- jsonlite::fromJSON(r)
+  if(!tidy) return(r)
+  tibble::as_tibble(r$response$facets)
+}
+
+.eia_facets_memoized <- memoise::memoise(.eia_facets)
+
+
+
+#' EIA data
+#'
+#' Obtain data from the EIA.
+#'
+#' By default, additional processing is done to return a list containing tibble data frames.
+#' Set `tidy = FALSE` to return only the initial list result of `jsonlite::fromJSON`.
+#' Set `tidy = NA` to return the original JSON as a character string.
+#'
+#' Set to `cache = FALSE` to force a new API call for updated data.
+#' Using `FALSE` always makes a new API call and returns the result from the server.
+#' `TRUE` uses memoization on a per R session basis, caching the result of the
+#' function call in memory for the duration of the R session.
+#' You can reset the entire cache by calling `eia_clear_cache`.
+#'
+#' @param dir character, directory path.
+#' @param data character vector,
+#' @param facets character list,
+#' @param frequency character
+#' @param start character
+#' @param end character
+#' @param tidy logical, return a tidier result. See details.
+#' @param cache logical, cache result for duration of R session using memoization. See details.
+#' @param key API key: character if set explicitly; not needed if key is set globally. See `eia_set_key()`.
+#'
+#' @return data.frame/tibble
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' eia_data(
+#'   dir = "electricity/retail-sales",
+#'   data = "price",
+#'   facets = list(sectorid = "RES", stateid = "OH")
+#' )
+#' }
+eia_data <- function(dir, data = NULL, facets = NULL, frequency = NULL, start = NULL, end = NULL, tidy = TRUE, cache = TRUE, key = eia_get_key()){
+  .key_check(key)
+  # if(cache) .eia_data_memoized(dir, data, facets, tidy, key) else .eia_data(dir, data, facets, tidy, key)
+  .eia_data(dir, data, facets, tidy, key)
+}
+
+.eia_data_url <- function(dir, data, facets, key){
+  dir <- .eia_url(path = paste0(dir, "/data/?api_key=", key))
+  data_specs <- if(!is.null(data)){
+    paste0("&data[]=", data, collapse = "")
+  }
+  facet_specs <- if(!is.null(facets)){
+    l <- lapply(
+      1:length(facets),
+      function(x) {
+        paste0(
+          "&facets[", names(facets[x]), "][]=", unlist(facets[x]),
+          collapse = ""
+        )
+      }
+    )
+    paste0(unlist(l), collapse = "")
+  }
+  paste0(dir, data_specs, facet_specs)
+}
+
+.eia_data <- function(dir, data, facets, tidy, key){
+  r <- .eia_data_url(dir, data, facets, key) |> .eia_get()
+  if(is.na(tidy)) return(r)
+  r <- jsonlite::fromJSON(r)
+  if(!tidy) return(r)
+  if (!is.null(r$response$warnings)){
+    wrngs <- paste0(r$response$warnings[[1]], "\n", r$response$warnings[[2]])
+    ttlrs <- r$response$total
+    warning(wrngs, "\nTotal available rows: ", ttlrs)
+  }
+  tibble::as_tibble(r$response$data)
+}
+
+.eia_data_memoized <- memoise::memoise(.eia_data)
