89109610223279c4ca01839793b1444865d19009
Matthew Hoff <matthew.g.hoff@gmail.com>
2023-10-11 14:17:48 -0400

    clean up format of Frequency table (format col), and re-arrange some things

--- R/metadata.R
+++ R/metadata.R
@@ -31,17 +31,6 @@ eia_metadata <- function(dir, tidy = TRUE, cache = TRUE, key = eia_get_key()){
   if(cache) .eia_metadata_memoized(dir, tidy, key) else .eia_metadata(dir, tidy, key)
 }
 
-.eia_metadata_url <- function(dir, key){
-  .eia_url(path = paste0(dir, "/?api_key=", key))
-}
-
-.eia_metadata_values <- function(data){
-  d <- sapply(names(data), function(x){
-    unlist(list("data" = x, data[[x]]))
-  })
-  tibble::as_tibble(t(d))
-}
-
 .eia_metadata <- function(dir, tidy, key){
   r <- .eia_metadata_url(dir, key) |> .eia_get()
   if(is.na(tidy)) return(r)
@@ -54,13 +43,13 @@ eia_metadata <- function(dir, tidy = TRUE, cache = TRUE, key = eia_get_key()){
       cat("\n\nDescription:\n  ", r$response$description)
     }
     if (!is.null(r$response$data)){
-      cat("\n\nData Values:\n  "); print(.eia_metadata_values(r$response$data))
+      cat("\n\nData Values:\n  "); print(.eia_data_values(r$response$data))
     }
     if (!is.null(r$response$facets)){
       cat("\nFacets:\n  "); print(tibble::as_tibble(r$response$facets))
     }
     if (!is.null(r$response$frequency)){
-      cat("\nFrequency:\n  "); print(tibble::as_tibble(r$response$frequency))
+      cat("\nFrequency:\n  "); print(.eia_freq_fmt(r$response$frequency))
     }
     cat("\nDefaults:\n  ")
     cat("Date Format:", r$response$defaultDateFormat, "\n  Frequency:", r$response$defaultFrequency)
@@ -70,6 +59,23 @@ eia_metadata <- function(dir, tidy = TRUE, cache = TRUE, key = eia_get_key()){
 }
 
 .eia_metadata_memoized <- memoise::memoise(.eia_metadata)
+
+.eia_metadata_url <- function(dir, key){
+  .eia_url(path = paste0(dir, "/?api_key=", key))
+}
+
+.eia_data_values <- function(data){
+  d <- sapply(names(data), function(x){
+    unlist(list("id" = x, data[[x]]))
+  })
+  tibble::as_tibble(t(d))
+}
+
+.eia_freq_fmt <- function(freq){
+  if ("format" %in% names(freq))
+    freq$format <- gsub('"', "", freq$format)
+  tibble::as_tibble(freq)
+}
 
 
 
@@ -118,4 +124,4 @@ eia_facets <- function(dir, facet, tidy = TRUE, cache = TRUE, key = eia_get_key(
   tibble::as_tibble(r$response$facets)
 }
 
-.eia_facets_memoized <- memoise::memoise(.eia_facets)
+.eia_facet_memoized <- memoise::memoise(.eia_facets)
