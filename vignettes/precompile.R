3eebf889bfa27c46780ccc6619c12507a2167b34
Matthew Hoff <matthew.g.hoff@gmail.com>
2023-10-13 15:53:39 -0400

    wrap up precompile

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,9 +1,11 @@
-# Precompiled vignettes that depend on API key
+# Pre-compiled vignettes that depend on API key
 # Must manually move image files from eia/ to eia/vignettes/ after knit
 
-library(knitr)
-knit("vignettes/api.Rmd.orig", "vignettes/api.Rmd")
-knit("vignettes/categories.Rmd.orig", "vignettes/categories.Rmd")
-knit("vignettes/eia.Rmd.orig", "vignettes/eia.Rmd")
-knit("vignettes/geoset.Rmd.orig", "vignettes/geoset.Rmd")
-knit("vignettes/series.Rmd.orig", "vignettes/series.Rmd")
+knitr::knit("vignettes/api.Rmd.orig", "vignettes/api.Rmd")
+knitr::knit("vignettes/data.Rmd.orig", "vignettes/data.Rmd")
+knitr::knit("vignettes/dirs.Rmd.orig", "vignettes/dirs.Rmd")
+knitr::knit("vignettes/eia.Rmd.orig", "vignettes/eia.Rmd")
+
+### DNE
+# knitr::knit("vignettes/facets.Rmd.orig", "vignettes/facets.Rmd")
+# knitr::knit("vignettes/metadata.Rmd.orig", "vignettes/metadata.Rmd")
