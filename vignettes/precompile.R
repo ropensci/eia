93cc71f9ca20dc120d2db0fc1928581a15a0934b
Matthew Hoff <matthew.g.hoff@gmail.com>
2023-10-24 17:44:03 -0400

    no need to compile vignettes that no longer exist

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,11 +1,5 @@
 # Pre-compiled vignettes that depend on API key
 # Must manually move image files from eia/ to eia/vignettes/ after knit
-
 knitr::knit("vignettes/api.Rmd.orig", "vignettes/api.Rmd")
-knitr::knit("vignettes/data.Rmd.orig", "vignettes/data.Rmd")
-knitr::knit("vignettes/dirs.Rmd.orig", "vignettes/dirs.Rmd")
 knitr::knit("vignettes/eia.Rmd.orig", "vignettes/eia.Rmd")
 
-### DNE
-# knitr::knit("vignettes/facets.Rmd.orig", "vignettes/facets.Rmd")
-# knitr::knit("vignettes/metadata.Rmd.orig", "vignettes/metadata.Rmd")
