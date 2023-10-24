497ed8e94761cefd8833c76fe18381295a97843f
Matthew Hoff <matthew.g.hoff@gmail.com>
2023-10-24 17:48:29 -0400

    rebase cleanup

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -2,4 +2,3 @@
 # Must manually move image files from eia/ to eia/vignettes/ after knit
 knitr::knit("vignettes/api.Rmd.orig", "vignettes/api.Rmd")
 knitr::knit("vignettes/eia.Rmd.orig", "vignettes/eia.Rmd")
-
