d3761364d9a41ba81f080403f6facdbc9878907f
leonawicz <mfleonawicz@gmail.com>
2023-10-30 11:11:27 -0600

    Add single line exclusion since that is all that's needed and the line number is unlikely to change any time soon for this context.

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,7 @@
 .onAttach <- function(libname, pkgname){
   options(eia_antidos = 1)
   if(Sys.getenv("EIA_KEY") == ""){
-    packageStartupMessage(
-      "EIA_KEY not found. See `vignette(\"api\", \"eia\")` for key storage options."
-    )
+    packageStartupMessage("EIA_KEY not found. See `vignette(\"api\", \"eia\")` for key storage options.")
   }
 }
 
